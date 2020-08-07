77218a071bcd219de1b75ab95b07174bcc459ae5
Snagi0 <47877391+Snagi0@users.noreply.github.com>
2020-08-08 01:11:24 +0300

    .

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -775,7 +775,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probing points, use the hotend as reference not the sensor.
-  #define TRAMMING_POINT_XY { {  0, 65 }, { 78,  38 }, { 59, 136 } }
+  #define TRAMMING_POINT_XY { {  35, 68 }, { 113,  41 }, { 94, 139 } }
 
   // Define positions names for probing points.
   #define TRAMMING_POINT_NAME_1 "Left"
