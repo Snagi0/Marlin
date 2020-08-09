8c4b0ebfc883599179433facd5f4f8295b5a83c5
Snagi0 <47877391+Snagi0@users.noreply.github.com>
2020-08-10 00:47:12 +0300

    .

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1327,8 +1327,8 @@
 
   #define MESH_EDIT_GFX_OVERLAY           // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 35                   // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 7             // Don't use more than 15 points per axis, implementation limited.
+  #define MESH_INSET 20                   // Set Mesh bounds as an inset region of the bed
+  #define GRID_MAX_POINTS_X 11             // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z           // Sophisticated users prefer no movement of nozzle
